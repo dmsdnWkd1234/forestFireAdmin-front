8c678ac4d134416319a76a992571a26693542083
kimeunwoo <gumman0620@gmail.com>
2025-04-12 18:03:00 +0900

    서버 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0', // 모든 IP에서 접근 가능
+        port: 3000, // 사용할 포트 번호
+    },
+});
