7bdfe5064df3692f4270c7108c34fd65f156e159
kimeunwoo <gumman0620@gmail.com>
2025-04-17 15:09:43 +0900

    쓸데없는 코드들 제거

--- src/map/MeshDataSelector.tsx
+++ src/map/MeshDataSelector.tsx
@@ -1,14 +1,11 @@
 import * as S from '../style/mesh/MeshDataSelector';
 
-import { useEffect, useState } from 'react';
-
 export default function MeshDataSelector() {
-    const [dataSelector, setDataSelector] = useState();
     const meshArray = ['온도', '습도', '이산화탄소', 'ㅇ', 'ㅇ', 'ㅇ'];
     return (
         <>
             <S.Root>
-                {meshArray.map((data, no) => (
+                {meshArray.map((data) => (
                     <>
                         <S.MeshDataTile>{data}</S.MeshDataTile>
                     </>
