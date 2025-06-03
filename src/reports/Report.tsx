e1411faa5100bc66d4b356a0f7fb2188e4575958
kimeunwoo <gumman0620@gmail.com>
2025-06-04 01:25:47 +0900

    역방향 프록시

--- src/reports/Report.tsx
+++ src/reports/Report.tsx
@@ -7,7 +7,7 @@ export default function Report() {
     const [reports, setReports] = useState<Report[]>([]);
 
     useEffect(() => {
-        fetch(`${import.meta.env.VITE_BACK_URL}api/report`)
+        fetch(`/api/report`)
             .then((res) => res.json())
             .then((data) => setReports(data))
             .catch((err) => console.error('공지 불러오기 실패:', err));
