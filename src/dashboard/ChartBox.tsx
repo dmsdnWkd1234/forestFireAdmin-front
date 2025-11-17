1ba81190f538a402aad16d67e7e607387eede112
kimeunwoo <gumman0620@gmail.com>
2025-11-17 13:44:22 +0900

    #20251117-1 버그 수정 1차

--- src/dashboard/ChartBox.tsx
+++ src/dashboard/ChartBox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as S from '../style/dashboard/chartBox';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
